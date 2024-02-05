2ea93b6cbadf1ab63f1fda5974e694d1cf1318f1
alfazsozib <alfazsozib.com>
2024-02-05 19:35:10 +0600

    video done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ function Hero() {
             
         </div>
         <div className="text_box relative w-[100%] flex flex-col items-center gap-4">
-          <h1 className="font-twister text-[40px] font-bold title ">superWeaves</h1>
+          <h1 className="font-twister text-[40px] font-bold title animate-title ">superWeaves</h1>
           <h1 className="font-superLagend text-4xl text tracking-widest">Trade, Bet & Game </h1>
           <h1 className="font-twister text-4xl bg-gradient-to-r from-[#f32f32] via-[#f46b59] to-[#f0e68d] text-transparent bg-clip-text">In One Web3 MMO Super APP</h1>
         </div>
