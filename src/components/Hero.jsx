f9be6bcdd8f8bd216e0982eb1262f37a322cf043
alfazsozib <alfazsozib.com>
2024-02-07 18:13:36 +0600

    text animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import MainBanner from "./images/main_banner.png";
 import logo from "../components/Video/public.gif";
 import BG1 from "../components/Video/Pearl_Texture_2.mp4";
+// import { Link } from "react-router-dom";
+
+
 function Hero() {
   return (
     <div className="max-w-[1200px] mx-auto flex w-full h-full overflow-hidden">
-      
       <div className="">
         <div className="banner-section">
           <div className="-z-10 absolute left-0 top-0 h-full w-full overflow-hidden">
@@ -18,35 +20,36 @@ function Hero() {
               loop
             ></video>
           </div>
-          
         </div>
 
-      
         <div className="logo relative">
           <a href="https://dwope.works/DWOOBz" rel="noreferrer">
             <img className="h-20 relative left-6 top-6" src={logo} alt="logo" />
             <div className="flex flex-col mt-60 gap-4 items-center ml-[100px]">
-                      <h1 className="font-twister text-[60px] font-bold title animate-title ">
-                    superWeaves
-                    </h1>
-                    <h1 className="font-superLagend text-[3.2rem] text tracking-widest">
-                      Trade, Bet & Game{" "}
-                    </h1>
-                    <h1 className="font-twister text-[3.2rem] bg-gradient-to-r from-[#f32f32] via-[#f46b59] to-[#f0e68d] text-transparent bg-clip-text">
-                      In One Web3 MMO Super APP
-                    </h1>
+            
+            
+            <div className="mx-auto md:mx-0 relative flex text-center items-center font-twist text-5xl tracking-tighter ">
+            <div>
+              <h1 className="linear-wipe font-twister text-5xl text-transparent  md:text-6xl">
+                SupperWaves
+              </h1>
+            </div>
+            <div className="absolute bottom-1 left-1 text-5xl md:text-6xl">
+              <h1 className="font-twister  text-[#E4E4E4]">SupperWaves</h1>
+            </div>
+          </div>
+
+              <h1 className="font-superLagend text-[3.2rem] text tracking-widest">
+                Trade, Bet & Game{" "}
+              </h1>
+              <h1 className="font-twister text-[3.2rem] linear-sub text-transparent bg-clip-text">
+                In One Web3 MMO Super APP
+              </h1>
             </div>
           </a>
 
-          <div className="text_box w-[100%] bg-red-300 middle-text flex flex-col items-center gap-4">
-          
+          <div className="text_box w-[100%] bg-red-300 middle-text flex flex-col items-center gap-4"></div>
         </div>
-        </div>
-
-
-        
-
-
       </div>
     </div>
   );
